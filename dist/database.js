be14d7700f24a2bd4a66c2eadc9956560246746e
Cesar <cjsseng@gmail.com>
2023-10-10 12:37:52 -0300

    Terminando o backend

--- dist/database.js
+++ dist/database.js
@@ -8,7 +8,11 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 // const MONGO_KEY = MONGO_PASSWORD;
 const MONGO_ADDRESS = process.env.MONGO_URL || "backup";
-mongoose_1.default.connect(MONGO_ADDRESS, {})
+mongoose_1.default.connect(MONGO_ADDRESS, {
+    writeConcern: {
+        w: 'majority'
+    }
+})
     .then(db => console.log('Database is connected'))
     .catch(err => console.log(err));
 //# sourceMappingURL=database.js.map
--- src/database.ts
+++ src/database.ts
@@ -6,7 +6,9 @@ dotenv.config();
 // const MONGO_KEY = MONGO_PASSWORD;
 const MONGO_ADDRESS = process.env.MONGO_URL || "backup";
 mongoose.connect(MONGO_ADDRESS, {
-
+    writeConcern: {
+        w: 'majority'
+      }
 })
     .then(db => console.log('Database is connected'))
     .catch(err => console.log(err));    
